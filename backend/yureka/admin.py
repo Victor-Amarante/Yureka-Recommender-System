c736d7b9615e765473bd3119b9a9a7729a5c4ac6
Victor Amarante <vafstats@gmail.com>
2025-03-10 22:31:06 -0300

    feat(admin): register models in Django Admin

--- backend/yureka/admin.py
+++ backend/yureka/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from yureka.domain.models import (
+  User, SocialAuth, Topic, UserTopic, Video, CuratedVideo, 
+  VideoTopic, UserRoutine, VideoMetric, Blacklist
+)
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(SocialAuth)
+admin.site.register(Topic)
+admin.site.register(UserTopic)
+admin.site.register(Video)
+admin.site.register(CuratedVideo)
+admin.site.register(VideoTopic)
+admin.site.register(UserRoutine)
+admin.site.register(VideoMetric)
+admin.site.register(Blacklist)
