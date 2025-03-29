13225c5f51ec7abd75e6604742a798b95ba8fab6
eliseucbrito <ecb2@cin.ufpe.br>
2025-03-29 16:08:42 -0300

    chore: set default App container styles

--- frontend/src/app/routes/app/root.tsx
+++ frontend/src/app/routes/app/root.tsx
@@ -1,16 +1,16 @@
 import { Outlet } from 'react-router';
 
-import { DashboardLayout } from '@/components/layouts';
-
 export const ErrorBoundary = () => {
   return <div>Something went wrong!</div>;
 };
 
 const AppRoot = () => {
   return (
-    <DashboardLayout>
-      <Outlet />
-    </DashboardLayout>
+    <div className="w-full h-full py-20">
+      <div className="w-full h-full max-w-[420px] sm:max-w-[576px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1200px] 2xl:max-w-[1440px] mx-auto">
+        <Outlet />
+      </div>
+    </div>
   );
 };
 
