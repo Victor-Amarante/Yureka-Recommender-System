12ea56867cebe992f2d25c155be8c6c0286fd0ad
eliseucbrito <ecb2@cin.ufpe.br>
2025-03-28 23:29:32 -0300

    chore: add shadcn and fonts

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,9 +1,11 @@
+import { Head } from '@/components/seo';
 import { AppProvider } from './provider';
 import { AppRouter } from './router';
 
 export const App = () => {
   return (
     <AppProvider>
+      <Head description="YuReka" />
       <AppRouter />
     </AppProvider>
   );
