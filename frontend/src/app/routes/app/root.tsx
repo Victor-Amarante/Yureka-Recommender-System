e49fa3d2896613e87690d64f46c911379d3f05c2
eliseucbrito <ecb2@cin.ufpe.br>
2025-03-30 00:04:47 -0300

    feat: add AppLayout

--- frontend/src/app/routes/app/root.tsx
+++ frontend/src/app/routes/app/root.tsx
@@ -1,3 +1,4 @@
+import AppLayout from '@/components/layouts/AppLayout';
 import { Outlet } from 'react-router';
 
 export const ErrorBoundary = () => {
@@ -6,11 +7,9 @@ export const ErrorBoundary = () => {
 
 const AppRoot = () => {
   return (
-    <div className="w-full h-full py-20">
-      <div className="w-full h-full max-w-[420px] sm:max-w-[576px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1200px] 2xl:max-w-[1440px] mx-auto">
-        <Outlet />
-      </div>
-    </div>
+    <AppLayout>
+      <Outlet />
+    </AppLayout>
   );
 };
 
---
+++ frontend/src/components/layouts/AppLayout/index.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from 'react';
+import AppNavbar from './AppNavbar';
+
+interface AppLayoutProps {
+  children: ReactNode;
+}
+
+export default function AppLayout({ children }: AppLayoutProps) {
+  return (
+    <div className="w-screen min-h-screen">
+      <AppNavbar />
+      <main className="flex-1 w-full py-20 max-w-relative">{children}</main>
+    </div>
+  );
+}
