2b9a8d3d6a3fb330861b2135ad5da92593611736
Victor Amarante <vafstats@gmail.com>
2025-04-03 20:29:58 -0300

    feat: add API routing for user and video-related endpoints, and include status check in yureka app

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('accounts/', include('allauth.socialaccount.urls'))
+    path('accounts/', include('allauth.socialaccount.urls')),
+    path('', include('yureka.urls'))
 ]
---
+++ backend/yureka/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    UserViewSet, TopicViewSet, UserTopicViewSet, VideoViewSet, CuratedVideoViewSet,
+    VideoTopicViewSet, UserRoutineViewSet, VideoMetricsViewSet, BlacklistViewSet,
+    status_check
+)
+
+# Criando um roteador para as rotas da API
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'topics', TopicViewSet)
+router.register(r'user-topics', UserTopicViewSet)
+router.register(r'videos', VideoViewSet)
+router.register(r'curated-videos', CuratedVideoViewSet)
+router.register(r'video-topics', VideoTopicViewSet)
+router.register(r'user-routines', UserRoutineViewSet)
+router.register(r'video-metrics', VideoMetricsViewSet)
+router.register(r'blacklist', BlacklistViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('api/status/', status_check, name='status_check'),
+]
